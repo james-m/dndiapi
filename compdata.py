6216d4d7bd60ea5a494914b48f0e82f23d4b6c91
Michael Barrett <mb@eventbrite.com>
2011-10-11 23:57:53 -0700

    Ok, we can auth against DDI Compendium and pull out the data.  Now we just need to parse the crap.

---
+++ compdata.py
@@ -0,0 +1,43 @@
+import mechanize
+import cookielib
+import BeautifulSoup
+from searchlib import VALID_CATEGORIES
+
+class InvalidLookup(Exception):
+    pass
+
+class CompendiumProxy(object):
+    """
+    Handles connecting & authenticating with the DDI Compendium as well as
+    looking up full data inside the Compendium.
+    """
+    BASE_URL = 'http://www.wizards.com/dndinsider/compendium'
+    LOOKUP_URL = BASE_URL + '/display.aspx?page=%s&id=%d'
+    PAGES = [x.lower() for x in VALID_CATEGORIES]
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+        self.browser = mechanize.Browser(factory=mechanize.RobustFactory())
+        self.browser.set_cookiejar(cookielib.LWPCookieJar())
+
+    def lookup(self, page, object_id):
+        """
+        Handles looking up data for a given page type & id.
+        """
+        if page not in self.PAGES:
+            raise InvalidLookup('%s not a valid page type.' % (page,))
+        response = self.browser.open(self.LOOKUP_URL % (page, object_id))
+        # Check to see if form1 has a password field.  If not, then we're
+        # authenticated already and we got the data we were looking for.  If so
+        # then we need to auth.
+        self.browser.select_form('form1')
+        try:
+            self.browser.form['email'] = self.email
+            self.browser.form['password'] = self.password
+        except mechanize._form.ControlNotFoundError:
+            # We're already authenticated, return the data
+            return response.read()
+        # We're not authenticated, so authenticate and then return the data
+        response = self.browser.submit()
+        return response.read()
